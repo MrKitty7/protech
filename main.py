19475627ebacc968229f2125ce9c3644177641a6
MrKitty7 <gbluedev@gmail.com>
2022-06-29 17:32:39 +0000

    updated everything

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import logging
 import disnake
 from disnake import message
 from disnake.ext import commands
 from disnake.ext.commands import command, has_permissions, bot_has_permissions
+import datetime
+import sqlite3
 
+
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
 # Intents ----------------------------------------------
 intents = disnake.Intents.all()
 
 # Bot Prefix & Intents Enable --------------------------
-client = commands.Bot(command_prefix='p!', intents=intents, test_guilds=[948245926290538506], help_command=None)
+client = commands.Bot(command_prefix='!', intents=intents, test_guilds=[948245926290538506], help_command=None)
+
 
 # Ping Command -----------------------------------------
 @client.command(description="Pong!")
@@ -38,13 +46,54 @@ async def help(ctx):
 # Kick Command ----------------------------------------
 @client.command(description="Kicks a member")
 @commands.has_permissions(kick_members=True)
-async def kick(ctx: commands.Context, member: disnake.Member, *, reason):
 
-    message = f"You have been kicked from **{ctx.guild.name}** for **{reason}**!"
-    await member.send(message)
+async def kick(ctx: commands.Context, member: disnake.Member = None, *, reason = None):
+    guildId = ctx.message.guild.id
+    guild = client.get_guild(guildId)
+    embed= disnake.Embed(
+        color=disnake.Color.blue(), title="**Notification**", description=f"You have been **kicked** from **``{guild.name}``** (ID:{guildId}) for **``{reason}``**")
+    embed.timestamp = datetime.datetime.utcnow()
+    
+    if member == None:
+         await ctx.send("You have to specify an user!")
+         print('no user')
+         return
+    if member == ctx.message.author or member == None:
+        await ctx.send("You cannot kick yourself!")
+        print('yourself')
+        return
+    if reason == None:
+        await ctx.send("You have to specify a reason!")
+        print('no reason')
+        return
+    
     await ctx.guild.kick(member, reason=reason)
     await ctx.send(f"{member} is succesfully kicked!")
+    await member.send(embed=embed)
     channel = disnake.utils.get(ctx.guild.channels, name="protech-logs")
     await channel.send(f"{member} has been **kicked** by {ctx.author} for a reason: **{reason}**")
     serverId = ctx.message.guild.id
-    await ctx.send(embed= disnake.Embed(title="Notification", description=f"You have been kicked from {serverId}")
+    
+@client.command(description="Sets up the bot. ")
+@commands.has_permissions(administrator=True)
+async def setup(ctx, mode: str):
+    guildId = ctx.message.guild.id
+    guild = client.get_guild(guildId) 
+    print(guildId)
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    if mode == 'logging':
+        print('yes')
+        logging_embed = disnake.Embed(color = disnake.Color.blue(),title = '**Logging setup**', description = 'Enter the ID of your logging channel.')
+        await ctx.send(embed=logging_embed)
+        logging_id = ctx.message.content
+        ctx.send('Done, set logging ID. ')
+        name = 'a' + str(guildId)
+        #cursor.execute(f"CREATE TABLE {name} (id INTEGER)")
+        cursor.execute(f"INSERT INTO {name} VALUES({logging_id}) ")
+        print(cursor.fetchall())
+        print(logging_id)
+    
+    
+
+client.run('OTc2OTI3OTMyMTM4MjkxMjUw.GwtY16.dxJ4pzazIgVTOaOihtDRxWYFYNlgxdeftiNzpg')
